629733d56c7232d3c32838ff88c4b16f419bbb67
tannv <nvantan94@gmail.com>
2021-07-17 22:07:58 +0900

    refactor code

--- src/main/java/de/niahealth/patient/survey/controller/SurveyController.java
+++ src/main/java/de/niahealth/patient/survey/controller/SurveyController.java
@@ -2,7 +2,6 @@ package de.niahealth.patient.survey.controller;
 
 import de.niahealth.patient.survey.constant.Paths;
 import de.niahealth.patient.survey.entity.Survey;
-import de.niahealth.patient.survey.service.PatientService;
 import de.niahealth.patient.survey.service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,13 +9,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class SurveyController {
     private SurveyService surveyService;
-    private PatientService patientService;
 
     @Autowired
-    public SurveyController(SurveyService surveyService,
-                            PatientService patientService) {
+    public SurveyController(SurveyService surveyService) {
         this.surveyService = surveyService;
-        this.patientService = patientService;
     }
 
     @RequestMapping(value = Paths.SURVEY_API, method = RequestMethod.POST)
--- src/main/java/de/niahealth/patient/survey/service/PatientService.java
+++
@@ -1,9 +0,0 @@
-package de.niahealth.patient.survey.service;
-
-import de.niahealth.patient.survey.entity.Patient;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface PatientService {
-    Patient retrievePatient(String username);
-}
