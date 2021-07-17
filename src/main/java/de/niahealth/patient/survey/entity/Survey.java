296383b970c740e857df24283d9f931433f520ae
tannv <nvantan94@gmail.com>
2021-07-17 21:34:14 +0900

    ignore patient in survey

--- src/main/java/de/niahealth/patient/survey/entity/Survey.java
+++ src/main/java/de/niahealth/patient/survey/entity/Survey.java
@@ -1,5 +1,6 @@
 package de.niahealth.patient.survey.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,6 +22,7 @@ public class Survey {
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "patientId", nullable = false)
+    @JsonIgnore
     private Patient patient;
 
     @Column(nullable = false)
