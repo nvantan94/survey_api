c71ed762d85df1ac774595b7bbcfd589d72a550d
tannv <nvantan94@gmail.com>
2021-07-18 11:39:37 +0900

    add test add existing survey unit test

--- src/test/java/de/niahealth/patient/survey/SurveyServiceUnitTest.java
+++ src/test/java/de/niahealth/patient/survey/SurveyServiceUnitTest.java
@@ -5,19 +5,20 @@ import de.niahealth.patient.survey.repository.PatientRepository;
 import de.niahealth.patient.survey.repository.SurveyRepository;
 import de.niahealth.patient.survey.service.SurveyService;
 import de.niahealth.patient.survey.service.impl.SurveyServiceImpl;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.validation.ConstraintViolationException;
 import java.util.ArrayList;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Survey service unit test")
@@ -39,13 +40,13 @@ public class SurveyServiceUnitTest {
     public void testAddValidSurvey() {
         Survey survey = new Survey(4, 5);
 
-        Mockito.when(surveyRepository.save(any(Survey.class))).thenReturn(survey);
+        when(surveyRepository.save(any(Survey.class))).thenReturn(survey);
 
         Survey insertedSurvey = surveyService.saveSurvey(survey);
 
-        Mockito.verify(surveyRepository).save(survey);
-        Assertions.assertEquals(survey.getLastNightSleep(), insertedSurvey.getLastNightSleep());
-        Assertions.assertEquals(survey.getSkinCondition(), insertedSurvey.getSkinCondition());
+        verify(surveyRepository).save(survey);
+        assertEquals(survey.getLastNightSleep(), insertedSurvey.getLastNightSleep());
+        assertEquals(survey.getSkinCondition(), insertedSurvey.getSkinCondition());
     }
 
     @Test
@@ -58,6 +59,21 @@ public class SurveyServiceUnitTest {
         invalidSurveys.add(new Survey(7, -2));
 
         for (var survey : invalidSurveys)
-            Assertions.assertThrows(ConstraintViolationException.class, () -> surveyService.saveSurvey(survey));
+            assertThrows(ConstraintViolationException.class, () -> surveyService.saveSurvey(survey));
+    }
+
+    @Test
+    @DisplayName("Test add existing survey")
+    public void testExistsSurvey() {
+        when(surveyRepository.existsTodaySurvey(1L)).thenReturn(true);
+        when(surveyRepository.existsTodaySurvey(2L)).thenReturn(false);
+
+        var exists1 = surveyService.existsTodaySurvey(1L);
+        var exists2 = surveyService.existsTodaySurvey(2L);
+
+        verify(surveyRepository).existsTodaySurvey(1L);
+        verify(surveyRepository).existsTodaySurvey(2L);
+        assertTrue(exists1);
+        assertFalse(exists2);
     }
 }
