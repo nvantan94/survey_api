18ec82fd84339f67ec1732ff534af98ff50b34fe
tannv <nvantan94@gmail.com>
2021-07-18 16:13:49 +0900

    add swagger 2

---
+++ src/main/java/de/niahealth/patient/survey/config/SwaggerConfiguration.java
@@ -0,0 +1,25 @@
+package de.niahealth.patient.survey.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("nia-health-api")
+                .apiInfo(
+                        new ApiInfoBuilder().title("Survey API")
+                        .description("Survey API reference for developers")
+                        .version("1.0").build()
+                )
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("de.niahealth.patient.survey.controller"))
+                .build();
+    }
+}
