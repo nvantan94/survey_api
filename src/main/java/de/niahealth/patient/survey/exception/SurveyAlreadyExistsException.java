c1e978f777ae99cc004ada79796694c1d448c7fb
tannv_57 <NVantan94@gmail.com>
2021-07-19 16:13:15 +0900

    add documentation for survey already exists exception

--- src/main/java/de/niahealth/patient/survey/exception/SurveyAlreadyExistsException.java
+++ src/main/java/de/niahealth/patient/survey/exception/SurveyAlreadyExistsException.java
@@ -1,5 +1,8 @@
 package de.niahealth.patient.survey.exception;
 
+/**
+ * <tt>SurveyAlreadyExistsException</tt> can be thrown as a patient tries to add a survey in a day in which there exists one already.
+ */
 public class SurveyAlreadyExistsException extends RuntimeException {
     public SurveyAlreadyExistsException() {
         super("You have already finished survey for today!");
