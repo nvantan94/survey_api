89442736da564172cc9b54b089d65a9dd148171c
tannv_57 <NVantan94@gmail.com>
2021-07-19 16:07:49 +0900

    add documentation for survey service

--- src/main/java/de/niahealth/patient/survey/service/SurveyService.java
+++ src/main/java/de/niahealth/patient/survey/service/SurveyService.java
@@ -3,8 +3,26 @@ package de.niahealth.patient.survey.service;
 import de.niahealth.patient.survey.entity.Survey;
 import org.springframework.stereotype.Component;
 
+/**
+ * This service acts as a bridge between
+ * {@link de.niahealth.patient.survey.controller.SurveyController SurveyController}
+ * and {@link de.niahealth.patient.survey.repository.SurveyRepository SurveyRepository}
+ */
 @Component
 public interface SurveyService {
+    /**
+     * Save a new survey, we also do some validations on survey data in this function.
+     * @param survey the survey user wants to save.
+     * @return inserted survey
+     *
+     * @throws javax.validation.ConstraintViolationException if any fields on the survey has a wrong value
+     */
     Survey saveSurvey(Survey survey);
+
+    /**
+     * Check if there exists a survey for a specific patient today.
+     * @param patientId the patientId you want to check for.
+     * @return <tt>true</tt> if database contains a survey for this patient in today.
+     */
     boolean existsTodaySurvey(long patientId);
 }
